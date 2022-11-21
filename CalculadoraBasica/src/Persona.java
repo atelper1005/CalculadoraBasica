594ad80a8b287c33884e3104afbc9ff2dd79f466
atelper1005 <116277422+atelper1005@users.noreply.github.com>
2022-11-21 10:48:32 +0100

    Add files via upload

---
+++ CalculadoraBasica/src/Persona.java
@@ -0,0 +1,17 @@
+public class Persona {
+    String nombre;
+
+    String apellidos;
+
+    int edad;
+
+    Persona() {
+        nombre = "Juan Carlos";
+        edad = 15;
+    }
+
+    Persona(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+}
